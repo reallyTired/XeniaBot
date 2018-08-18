3474dd1f3373ce13c73baedf868b66098d0cb073
reallyTired <42482334+reallyTired@users.noreply.github.com>
2018-08-18 04:54:05 +0100

    Added the bots source code at its first release format
    Token and Role ID removed so replace them in the appropriate places to use the code yourself

---
+++ Xenia bot.py
@@ -0,0 +1,48 @@
+
+import discord # Import Discord API library
+import asyncio # Import the Async library
+import random # Imports psuedo random number generation library
+
+Token = '' # The token that connects it to the bot account 
+
+Wordbank1 = ['fuckin ','piece of ','stupid ','dumb-ass ','slow '] # First set of words for insult generator
+Banklen1 = len(Wordbank1) # Totals the number of words in the first wordbank
+Wordbank2 = ['cunt','wanker','cuck','fucker','shit'] # Second set of words for insult generator
+Banklen2 =len(Wordbank1) # Totals the number of words in the second wordbank
+
+class MyClient(discord.Client):
+    async def on_ready(self): # Prints basic bot info to console
+        print('------')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+    async def on_message(self, message):
+
+        if message.author == self.user: # Stops it responding to itself
+            return
+
+        if message.content.startswith('!help'): # Command that lists all BOT commands
+            await client.send_message(message.channel,'All commands: \n • !help - Provides this help text \n • !source - Links BOT source code \n • !game - Links all the games the server is relevent to \n • !badrun - Generates an insult and @\'s baku so you dont have to \n • !goodbye - closes the bot'.format(message))
+
+        if message.content.startswith ('!source'): # Command that links to Bot source code github repo
+            await client.send_message (message.channel,'https://github.com/reallyTired/XeniaBot'.format(message))
+            
+        if message.content.startswith ('!game'): # Posts links to all the Xenia games
+           await client.send_message (message.channel,'https://baku.itch.io/legend-of-xenia'.format(message))
+           await client.send_message (message.channel,'https://baku.itch.io/legend-of-xenia-2'.format(message))
+           await client.send_message (message.channel,'https://baku.itch.io/legend-of-xenia-3d'.format(message))
+
+        if message.content.startswith ('!badrun'): # Generates a random insult at baku
+            await client.send_message (message.channel,'@baku#3399 ' + Wordbank1[random.randint(1,Banklen1)] + Wordbank2[random.randint(1,Banklen2)].format(message))
+
+        if message.content.startswith ('!goodbye'): # Lets users with the 'Admin' role turn off the BOT
+            if '' in [role.id for role in message.author.roles]:
+                await client.send_message (message.channel,'Goodbye all!'.format(message))
+                await client.logout()
+            else:
+                await client.send_message (message.channel,'Sorry, not gonna work, Only admin can turn me off'.format(message))
+
+        
+client = MyClient()
+client.run(Token)
