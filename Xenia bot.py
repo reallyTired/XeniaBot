50415b24ae2ba9e49a928e88554eae96d04cff3c
reallyTired <42482334+reallyTired@users.noreply.github.com>
2018-08-18 05:08:43 +0100

    very very minor changes
    !help command now puts the commands in code block to highlight it

--- Xenia bot.py
+++ Xenia bot.py
@@ -3,7 +3,7 @@ import discord # Import Discord API library
 import asyncio # Import the Async library
 import random # Imports psuedo random number generation library
 
-Token = '' # The token that connects it to the bot account 
+Token = '' # The token that connects it to the bot account [YOU MUST REPLACE TOKEN HERE]
 
 Wordbank1 = ['fuckin ','piece of ','stupid ','dumb-ass ','slow '] # First set of words for insult generator
 Banklen1 = len(Wordbank1) # Totals the number of words in the first wordbank
@@ -23,7 +23,7 @@ class MyClient(discord.Client):
             return
 
         if message.content.startswith('!help'): # Command that lists all BOT commands
-            await client.send_message(message.channel,'All commands: \n • !help - Provides this help text \n • !source - Links BOT source code \n • !game - Links all the games the server is relevent to \n • !badrun - Generates an insult and @\'s baku so you dont have to \n • !goodbye - closes the bot'.format(message))
+            await client.send_message(message.channel,'All commands: \n ```• !help - Provides this help text \n • !source - Links BOT source code \n • !game - Links all the games the server is relevent to \n • !badrun - Generates an insult and @\'s baku so you dont have to \n • !goodbye - closes the bot```'.format(message))
 
         if message.content.startswith ('!source'): # Command that links to Bot source code github repo
             await client.send_message (message.channel,'https://github.com/reallyTired/XeniaBot'.format(message))
@@ -37,7 +37,7 @@ class MyClient(discord.Client):
             await client.send_message (message.channel,'@baku#3399 ' + Wordbank1[random.randint(1,Banklen1)] + Wordbank2[random.randint(1,Banklen2)].format(message))
 
         if message.content.startswith ('!goodbye'): # Lets users with the 'Admin' role turn off the BOT
-            if '' in [role.id for role in message.author.roles]:
+            if '' in [role.id for role in message.author.roles]: # [YOU MUST REPLACE THE ADMIN OR EQUIVILENT ROLE ID HERE]
                 await client.send_message (message.channel,'Goodbye all!'.format(message))
                 await client.logout()
             else:
