55bc5794f7903f240cc74f8a5162f07e350e9ad9
Jhynjhiruu <cub3r12@gmail.com>
2018-08-19 21:31:05 +0100

    Added srcomapi stuff

--- Xenia bot.py
+++ Xenia bot.py
@@ -5,6 +5,23 @@ import asyncio # Import the Async library
 import random # Imports psuedo random number generation library
 import re # Imports pattern stuff
 
+try:
+    import srcomapi, srcomapi.datatypes as src
+except:
+    print("Please run 'pip install srcomapi' to use this bot")
+
+api = srcomapi.SpeedrunCom()
+
+games = api.search(srcomapi.datatypes.Game, {"name": "legend of xenia"})
+
+game_ids = []
+
+for i in games:
+    game_ids.append([j for j in i.categories])
+
+if len(game_ids) < 1:
+    raise Exception ("Could not connect to API")
+
 prefixes = ['+', '!', 'n!']
 
 admins = ['Admin', 'VAC B&']
@@ -18,6 +35,9 @@ try:
     Token = f
 except Exception as e:
     raise Exception (str(e) + ": please provide a valid Discord token in the token.tkn file!")
+
+def formatTime(time):
+    return str(int(time // 60)), str(int(time % 60)).zfill(2), str(time % 1).lstrip("0.")[:3]
 
 class MyClient(discord.Client):
     async def on_ready(self): # Prints basic bot info to console
@@ -61,10 +81,26 @@ class MyClient(discord.Client):
 
     async def wr(message, *args): # posts Any% WR for desired game
         if args[0] == 'lox1':
-            await client.send_message(message.channel, 'LoX1 wr not implemented yet')
+            if args[1] == "any%":
+                time = game_ids[0][0].records[0].runs[0]["run"].times["primary_t"]
+                await client.send_message(message.channel, "WR for LoX1 any%: {}:{}.{:<03}".format(*formatTime(time)))
+                
+            elif args[1] == "100%":
+                print("100% selected")
+                time = game_ids[0][1].records[0].runs[0]["run"].times["primary_t"]
+                await client.send_message(message.channel, "WR for LoX1 100%: {}:{}.{:<03}".format(*formatTime(time)))
             
+            else:
+                print("invalid choice")
+                await client.send_message(message.channel, "{} is not a category for LoX1".format(args[1]))
+                
         elif args[0] == 'lox2':
-            await client.send_message(message.channel, 'LoX2 wr not implemented yet')
+            if args[1] == "any%":
+                time = game_ids[1][0].records[0].runs[0]["run"].times["primary_t"]
+                await client.send_message(message.channel, "WR for LoX2 any%: {}:{}.{:<03}".format(*formatTime(time)))  
+                
+            else:
+                await client.send_message(message.channel, "{} is not a category for LoX2".format(args[1]))            
             
         else:
             await client.send_message(message.channel, args[0] + ' wr not implemented yet')
